6204adfb516b165f9fc1a8c9b6331e5e96a67cc0
Pete Kaffenberger <pete.kaffenberger@gmail.com>
2021-03-21 21:46:39 -0400

    commented layout for functions and loops

--- Develop/script.js
+++ Develop/script.js
@@ -15,6 +15,8 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -22,6 +24,42 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "<", ">", "/", "?"];
+
+
+//function to generate password length, prompt to gather desired number of characters
+function generatePassword () {
+
+var userPasswordLength = prompt("How long would you like your password to be? (Password must be between 8 and 128 characters")
+
+//if statement for 
+
+var userInput = [];
+
+var userPassword = "";
+
+var userPassword = "";
+
+}
+
+
+
+//function to generate uppercase letters in password, confirmation prompt
+
+
+
+//funciton to generate lowercase letters in password, confirmation prompt
+
+
+
+//function to generate numbers in password, confirmation prompt
+
+
+
+//function to generate special characters in password, confirmation prompt
+
+
+
+//loop to check that a character type was chosen and password has between 8 & 128 characters
 
 
 
